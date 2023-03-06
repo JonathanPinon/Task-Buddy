6502e90ba49a91d787cc2fd69bfdec1db0d333b6
jonathan pinon <jonathanjosephpinon@gmail.com>
2023-03-06 15:54:36 -0800

    Feature 10 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("login/", views.user_login, name="login"),
     path("logout/", views.user_logout, name="logout"),
+    path("signup/", views.signup, name="signup")
 ]
