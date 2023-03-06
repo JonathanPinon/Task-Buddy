fe6b623a5cbf34651a01b81a735ec84b80e27b9d
jonathan pinon <jonathanjosephpinon@gmail.com>
2023-03-06 15:41:59 -0800

    Feature 7 complete

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from projects.models import Project
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
+@login_required
 def list_projects(request):
     projects = Project.objects.all()
     context = {
