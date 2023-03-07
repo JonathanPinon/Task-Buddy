c3471c856addb6a0cf214fc55845ebdc543600a2
jonathan pinon <jonathanjosephpinon@gmail.com>
2023-03-06 18:05:38 -0800

    Feature 14 complete

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.list_projects, name="list_projects"),
     path("<int:id>/", views.show_project, name="show_project"),
+    path("create/", views.create_project, name="create_project"),
 ]
